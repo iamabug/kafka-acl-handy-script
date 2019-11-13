fab4d30608fb26d3bc7e18480901ec774f33feb8
iamabug <mm1201@mail.ustc.edu.cn>
2019-11-13 19:29:00 +0800

    Update trans_producer_add.sh

--- trans_producer_add.sh
+++ trans_producer_add.sh
@@ -9,7 +9,9 @@ if [[ $PREFIXED == 0 ]]; then
     ${ACL_CMD_PREFIX} --add --allow-principal User:${USERNAME} --producer --topic ${TOPIC} --transactional-id "*" > /dev/null 2>&1
 else
     echo "role: prefixed transactional producer"
-    ${ACL_CMD_PREFIX} --add --allow-principal User:${USERNAME} --producer --topic ${TOPIC} --resource-pattern-type prefixed --transactional-id "*" > /dev/null 2>&1
+    ${ACL_CMD_PREFIX} --add --allow-principal User:${USERNAME} --producer --topic ${TOPIC} --resource-pattern-type prefixed > /dev/null 2>&1
+    [[ $? != 0]] && (echo "status: FAILED"; exit 1)
+    ${ACL_CMD_PREFIX} --add --allow-principal User:${USERNAME} --transactional-id "*" > /dev/null 2>&1
 fi
 
 [[ $? == 0 ]] && echo "status: SUCCEED" || echo "status: FAILED"
